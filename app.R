33e864269c3530ef9a659ab44c640f5c5817f8a9
Wellington Ferreira de Souza <wellingtonf.souza@outlook.com>
2019-10-05 22:07:00 -0300

    começo do aplicativo

---
+++ app.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(plotly)
+
+header = dashboardHeader(title = "ENEM Analytics")
+
+sidebar = dashboardSidebar(
+  sidebarMenu(
+    menuItem("Início",tabName = "inicio",icon = icon("home")),
+    menuItem("Metodologia",tabName = "metodologia",icon = icon("fas fa-info-circle")),
+    menuItem("Análise geral",tabName = "analise_geral",icon = icon("fas fa-chart-bar")),
+    menuItem("Condições especiais",tabName = "cond_especiais",icon = icon("fas fa-user-tag")),
+    menuItem("LinkedIn",icon = icon("fab fa-linkedin"),
+             href = "https://www.linkedin.com/in/wellington-ferr-souza/"),
+    menuItem("Github",icon = icon("fab fa-github"),
+             href = "https://github.com/wellingtonf-souza/")
+  )
+)
+  
+body = dashboardBody(
+  tags$head(tags$style(HTML('
+      .main-header .logo {
+        font-family: "Caviar Dreams", Arial;
+        font-weight: bold
+      }
+    '))),
+  tags$li(tags$style(HTML('li { font-family: "Caviar Dreams", Arial; }'))),
+  tags$div(tags$style(HTML('.box-header h3 { font-family: "Caviar Dreams", Arial;}'))),
+  tags$div(tags$style(HTML('div { font-family: "Caviar Dreams", Arial;}'))
+           ),
+  tabItems(
+    tabItem(tabName = "inicio",
+            HTML('<center><img src="enem_logo.png" class="img-responsive" width=80% height=80% ></center>'),
+            class="active")
+  )
+  
+)
+
+ui = dashboardPage(
+  header,
+  sidebar,
+  body
+)
+server = function(input,output,session){
+  
+}
+
+shinyApp(ui,server)
