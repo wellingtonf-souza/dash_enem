6f65ac0afadb61ebe33b0479e4288a88fe888c4c
Wellington Ferreira de Souza <wellingtonf.souza@outlook.com>
2019-10-08 01:25:39 -0300

    novas analises no aplicativo

--- app.R
+++ app.R
@@ -2,6 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(plotly)
+library(highcharter)
+library(shinycssloaders)
 
 amostra   = readRDS("amostra_2014_2017.rds")
 nec_espec = readRDS("nec_espec_2014_2017.rds")
@@ -12,7 +14,8 @@ sidebar = dashboardSidebar(
   sidebarMenu(
     menuItem("Início",tabName = "inicio",icon = icon("home")),
     menuItem("Metodologia",tabName = "metodologia",icon = icon("fas fa-info-circle")),
-    menuItem("Análise geral",tabName = "analise_geral",icon = icon("fas fa-chart-bar")),
+    menuItem("Análise geral",tabName = "analise_geral",icon = icon("fas fa-chart-bar"),
+             menuSubItem("Univariada",tabName = "univ")),
     menuItem("Condições especiais",tabName = "cond_especiais",icon = icon("fas fa-user-tag")),
     menuItem("LinkedIn",icon = icon("fab fa-linkedin"),
              href = "https://www.linkedin.com/in/wellington-ferr-souza/"),
@@ -67,6 +70,42 @@ body = dashboardBody(
               a(href = "https://cran.r-project.org/","R"),"e pacote",
               a(href = "https://rstudio.github.io/shinydashboard/", "shinydashboard.")
            )
+    ),
+    tabItem(tabName = "univ",
+            fluidRow(column(width = 2,
+                            selectInput(inputId = "nota.geral",
+                                        label = "Selecione uma nota:",
+                                        choices = c("Ciências da Natureza"="NOTA_CN",
+                                                    "Ciências Humanas"="NOTA_CH",
+                                                    "Linguagens e Códigos" = "NOTA_LC",
+                                                    "Matemática" = "NOTA_MT",
+                                                    "Redação" = "NOTA_REDACAO"))),
+                     column(width = 2,
+                            selectInput(inputId = "ano.geral",
+                                        label = "Selecione um ano:",
+                                        choices = 2014:2017)),
+                     column(width = 5,
+                            selectInput(inputId = "var.geral",
+                                        label = "Selecione uma variável:",
+                                        choices = c("Dependência administrativa"="DEPENDENCIA_ADM_ESC",
+                                                    "Localização"="LOCALIZACAO_ESC",
+                                                    "Sexo"="SEXO",
+                                                    "Situação de conclusão do Ensino Médio "="ST_CONCLUSAO",
+                                                    "Cor/Raça"="COR_RACA",
+                                                    "Até que série o pai, ou o homem responsável, estudou"="Q001",
+                                                    "Até que série a mãe, ou a mulher responsável, estudou"="Q002",
+                                                    "Renda mensal da família"="Q006",
+                                                    "Tipo de escola que frequentou o Ensino Médio"="Q027")))
+            ),
+            fluidRow(box(width = 12,
+                         title = "Boxplots das notas em relação às categorias da variável",
+                         withSpinner(plotlyOutput("graf.boxplot.geral")))),
+            fluidRow(box(width = 12,
+                         title = "Densidade das notas em relação às categorias da variável",
+                         withSpinner(plotlyOutput("graf.density.geral")))),
+            fluidRow(box(width = 12,
+                         title = "Quantitativo de indivíduos em relação às categorias da variável",
+                         withSpinner(plotlyOutput("graf.quanti.geral"))))
     )
   )
 )
@@ -76,6 +115,46 @@ ui = dashboardPage(
   body
 )
 server = function(input,output,session){
+  
+  #=====================================
+  # analise geral
+  #=====================================
+  output$graf.boxplot.geral = renderPlotly({
+    g1 = amostra %>% 
+      dplyr::select(input$nota.geral,input$var.geral,ANO) %>% 
+      dplyr::filter(ANO==input$ano.geral) %>% na.omit() %>% 
+      ggplot()+
+      geom_boxplot(mapping = aes(!!sym(input$var.geral),
+                                 !!sym(input$nota.geral),
+                                 fill = !!sym(input$var.geral))) +
+      theme_minimal() + 
+      theme(legend.position='none') + coord_flip() + labs(fill = "") + xlab("") + ylab("")
+      ggplotly(g1)
+  })
+  
+  output$graf.density.geral = renderPlotly({
+    g2 = amostra %>% 
+      dplyr::select(input$nota.geral,input$var.geral,ANO) %>% 
+      dplyr::filter(ANO==input$ano.geral) %>% na.omit() %>% 
+      ggplot()+
+      geom_density(mapping = aes(!!sym(input$nota.geral),
+                                 fill = !!sym(input$var.geral)),alpha = 0.5) +
+      theme_minimal() + labs(fill = "") + xlab("") + ylab("")
+    ggplotly(g2)
+  })
+  
+  output$graf.quanti.geral = renderPlotly({
+    set.seed(12345)
+    g3 = amostra %>% 
+      dplyr::select(input$nota.geral,input$var.geral,ANO) %>% 
+      dplyr::filter(ANO==input$ano.geral) %>% na.omit() %>% 
+      ggplot(mapping = aes(!!sym(input$var.geral)))+
+      geom_bar(aes(fill=!!sym(input$var.geral)),col = "black") + 
+      theme_minimal() + labs(fill = "") +
+      theme(legend.position='none') +
+      coord_flip() + xlab("") + ylab("")
+    ggplotly(g3,tooltip = c("x","y"))
+  })
   
 }
 
